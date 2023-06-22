0bc18b90a5a841f36e12f037745b3a34205e9fe0
Andrew Murrr <murrr.andrew@gmail.com>
2023-06-23 00:43:07 +0300

    WP: styles for basic gutenberg block

--- wp-content/plugins/basic-gutenberg-plugin/blocks/basic-block/build/index.asset.php
+++ wp-content/plugins/basic-gutenberg-plugin/blocks/basic-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '85adb78cc76edf3603a9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f09b90ec2f51b048a764');
