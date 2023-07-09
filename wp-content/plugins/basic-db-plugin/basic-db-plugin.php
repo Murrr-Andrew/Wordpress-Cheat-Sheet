03464f4a9adf54d9b07a00f1acac44ec37ca8cd9
Andrew Murrow <murrr.andrew@gmail.com>
2023-07-09 19:37:46 +0300

    WP: Basic db plugin

---
+++ wp-content/plugins/basic-db-plugin/basic-db-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Basic DB Plugin
+ * Description: Custom plugin with db examples.
+ * Version: 1.0.0
+ * Author: Murrr Andrew
+ * License: GNU General Public License v2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'BDBP_URL', plugins_url('', __FILE__) );
+define( 'BDBP_PATH', plugin_dir_path(__FILE__) );
+
+require_once __DIR__ . '/classes/autoload.php';
+
+add_action( 'plugins_loaded', 'BasicDBPlugin\Airport::init' );
+
+register_activation_hook( __FILE__, ['BasicDBPlugin\Airport', 'plugin_activation'] );
---
+++ wp-content/plugins/basic-db-plugin/classes/BasicDBPlugin/Airport.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace BasicDBPlugin;
+
+class Airport 
+{
+    public static function init()
+    {
+        add_action( 'wp_ajax_airport_action', [__CLASS__, 'airport_action_callback'] );
+        add_action( 'wp_ajax_nopriv_airport_action', [__CLASS__, 'airport_action_callback'] );
+        add_action( 'wp_enqueue_scripts', [__CLASS__, 'plugin_scripts'] );
+    }
+
+    public static function plugin_scripts()
+    {
+        wp_enqueue_script( 'basic_db_plugin_script', BDBP_URL . '/assets/js/script.js' , array(), null );
+
+        wp_localize_script( 'basic_db_plugin_script', 'bdbpData', [
+            'ajax_url' => admin_url( 'admin-ajax.php' ),
+            'ajax_nonce' => wp_create_nonce( 'bdbp_ajax_nonce' ),
+        ]);
+    }
+
+    public static function airport_action_callback()
+    {
+        global $wpdb;
+
+        $post = stripslashes_deep( $_POST );
+
+        if ( ! isset( $post['nonce'] ) || ! wp_verify_nonce( $post['nonce'], 'bdbp_ajax_nonce' ) ) {
+            wp_send_json_error( 'Invalid or missing nonce' );
+        }
+
+        if ( !isset( $post['iata'] ) ) {
+            wp_send_json_error( 'Invalid data provided' );
+        }
+
+        $iata = sanitize_text_field( $post['iata'] );
+
+        $response = array();
+
+        try {
+            $prepared_query = $wpdb->prepare( "SELECT * FROM airports WHERE iata = %s", $iata );
+            $results = $wpdb->get_results( $prepared_query );
+
+            if ( !empty( $results ) ) {
+                $result = $results[0];
+                $response['iata'] = $result->iata;
+                $response['airport'] = $result->airport;
+            } else {
+                wp_send_json_error( 'Airport not found.' );
+            }
+        } catch ( Exception $e ) {
+            wp_send_json_error( 'An error occurred while retrieving airport: ' . $e->getMessage() );
+        }
+
+        wp_send_json_success( json_encode($response), true );
+    }
+
+    public static function plugin_activation() 
+    {
+        global $wpdb;
+
+        $sql_file = BDBP_PATH . 'db/airports.sql';
+
+        if ( file_exists( $sql_file ) ) {
+            $sql_contents = file_get_contents( $sql_file );
+
+            $table_name = $wpdb->prefix . 'airports';
+            $drop_table_sql = "DROP TABLE IF EXISTS $table_name;";
+            $wpdb->query( $drop_table_sql );
+
+            require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+            dbDelta( $sql_contents );
+        }
+    }
+}
